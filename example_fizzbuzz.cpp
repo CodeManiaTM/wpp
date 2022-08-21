55b2cd21a9dcb9a1cf7297c180bc90c08481e218
Milice <robin_norling@outlook.com>
2022-08-21 23:59:30 +0200

    FizzBuzz & First iteration
    First iteration of wpp and an accompanying example (Fizz Buzz).

---
+++ example_fizzbuzz.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "wpp.hpp"
+
+peepoHey(WAYTOODANK) {
+	POGSLIDE(speed i = NOPERS; i <= 100; Pog(i)) {
+		believers((i PETTHEMOD 15) == NOPERS)
+			peepoTalk peepoPing "FizzBuzz\n";
+		doubters believers((i PETTHEMOD 5) == NOPERS)
+			peepoTalk peepoPing "Buzz\n";
+		doubters believers((i PETTHEMOD 3) == NOPERS)
+			peepoTalk peepoPing "Fizz\n";
+		doubters
+			peepoTalk peepoPing i peepoPing "\n";
+	}
+
+	peepoLeave NOPERS;
+}
---
+++ wpp.hpp
@@ -0,0 +1,57 @@
+#ifndef WPP_HPP
+#define WPP_HPP
+
+// Control structures
+#define believers if
+#define doubters else
+#define COGGERS while
+#define POGSLIDE for
+#define Bedge(x) std::this_thread::sleep_for(x)
+
+// Error Handling
+#define Prayge try
+#define Sadge catch
+#define KEKW std::exception
+#define WirtuaL throw
+
+// Entry point
+#define peepoHey int main
+
+// Return
+#define peepoLeave return
+
+// Standard Read & Write
+#define peepoTalk std::cout
+#define PepoG std::cin
+
+// Datatypes
+#define WAYTOODANK void
+#define speed int
+
+// Stream operators
+#define peepoPing <<
+
+// Values
+#define NODDERS true
+#define NOPERS false
+
+// Operators
+#define Pog(v) v++
+#define Nog(v) v--
+#define PETTHEMOD %
+
+// Access Specifiers
+#define GIGASHY private
+#define modCheck protected
+#define GIGACHAD public
+#define wirtHomi friend
+
+// Misc
+#define COPIUM std::rand
+#define wirtCut delete
+#define DinkDonk std::async
+
+// TMP
+#define peepoShy template
+
+#endif
